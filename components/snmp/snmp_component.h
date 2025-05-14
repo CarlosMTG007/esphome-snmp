14454c0e68126dc75e0e6f55ad01c69195c48f57
CarlosMTG007 <pg8352482@gmail.com>
2025-05-14 11:47:16 +0100

    Update snmp_component.h

--- components/snmp/snmp_component.h
+++ components/snmp/snmp_component.h
@@ -18,11 +18,12 @@ namespace esphome {
 namespace snmp {
 
 /// The SNMP (Simple Network Management Protocol) component provides support for collecting and organizing
-/// information about managed devices on a networks.
+/// information about managed devices on a network.
 
 class SNMPComponent : public Component {
  public:
-  SNMPComponent() : snmp_agent_("public", "private"){};
+  SNMPComponent() : snmp_agent_("public", "private") {}
+
   void setup() override;
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::AFTER_WIFI; }
@@ -31,42 +32,37 @@ class SNMPComponent : public Component {
   void set_contact(const std::string &contact) { contact_ = contact; }
   void set_location(const std::string &location) { location_ = location; }
 
-  void setup_sensor_mib_();
   void set_sensor_values(float temperature, float humidity, float lux);
 
  protected:
   WiFiUDP udp_;
   SNMPAgent snmp_agent_;
 
+  // MIB setup methods
   void setup_system_mib_();
   void setup_storage_mib_();
-#ifdef USE_ESP32
-  void setup_esp32_heap_mib_();
-#endif
-#ifdef USE_ESP8266
-  void setup_esp8266_heap_mib_();
-#endif
   void setup_chip_mib_();
   void setup_wifi_mib_();
+  void setup_sensor_mib_();
+
 #ifdef USE_ESP32
+  void setup_esp32_heap_mib_();
   static int setup_psram_size(int *used);
-#endif
-
-  static uint32_t get_uptime() { return millis() / 10; }
-  static uint32_t get_net_uptime();
-  static std::string get_bssid();
-
-#ifdef USE_ESP32
   static int get_ram_size_kb();
 #endif
 
-  /// contact string
-  std::string contact_;
+#ifdef USE_ESP8266
+  void setup_esp8266_heap_mib_();
+#endif
 
-  /// location string
+  static uint32_t get_uptime();
+  static uint32_t get_net_uptime();
+  static std::string get_bssid();
+
+  std::string contact_;
   std::string location_;
 
-  /// sensor values
+  // Sensor data for SNMP
   float temperature_{NAN};
   float humidity_{NAN};
   float lux_{NAN};
